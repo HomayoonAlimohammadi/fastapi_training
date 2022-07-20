3f30308a74defb573dbf4a39ff096c7acbdef9db
Homayoon Alimohammadi <homayoonalimohammadi@gmail.com>
2022-07-20 13:21:47 +0430

    Update fastapi tutorial and training

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, Path, Query
 from dataclasses import dataclass
 from enum import Enum
 import pydantic
@@ -11,9 +11,23 @@ class Models(Enum):
     lenet = "lenet"
 
 
+class Image(pydantic.BaseModel):
+    url: pydantic.HttpUrl
+    name: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "url": "https://target.website.com/path/to/image",
+                "name": "that same kitty again!",
+            }
+        }
+
+
 class Item(pydantic.BaseModel):
     name: str
-    id: int
+    id: int = pydantic.Field(ge=1, title="ID of an Item.")
+    image: Image | None = None
 
 
 @dataclass
@@ -47,8 +61,83 @@ def test(test_var: bool = False):
 
 
 @app.post("/items/")
-def create_item(item: Item):
+def create_item(
+    item: Item = Body(
+        examples={
+            "normal": {
+                "summary": "acceptable example",
+                "description": "this is how a true Item should look like!",
+                "value": {
+                    "item": {
+                        "name": "nice item",
+                        "id": "10",
+                        "image": {
+                            "url": "https://www.images.com/path/to/image",
+                            "name": "nice image",
+                        },
+                    }
+                },
+            },
+            "bad": {
+                "summary": "a bad example",
+                "description": "this is how a bad Item should look like!",
+                "value": {
+                    "item": {
+                        "name": "a;lsgjsdlfj",
+                        "id": "-10",
+                        "image": {
+                            "url": "a;dlkfja or /path/to/image alone",
+                            "name": "aflaksdjfl",
+                        },
+                    }
+                },
+            },
+        },
+    )
+):
     return {"creation_date": datetime.now(), **item.dict()}
+
+
+@app.get("/greet/")
+async def greet(name: str | None = Query(default=None, max_length=10)):
+    if name:
+        return f"Greetings {name}!"
+    return "Greetings!"
+
+
+# if you want to declare a query parameter of type "list",
+# you should use "Query". Otherwise it will be considered a request body.
+# Because only singular types are primarily considered a query parameter.
+@app.get("/greets/")
+async def greets(names: list[str] | None = Query(default=None)):
+    if names:
+        names = ", ".join(names)
+        return f"Greetings {names}!"
+    return "Greetings!"
+
+
+@app.get("/say-hello/")
+async def say_hello(
+    name: str = Query(
+        default="dear nooshin joon",
+        title="User Name",
+        alias="user-name",
+        deprecated=True,
+        min_length=3,
+        max_length=20,
+        regex="nooshin",
+    )
+):
+    return f"Hello {name}"
+
+
+@app.get("/items/{item_id}")
+async def get_item_by_id(
+    item_id: int = Path(title="ID of the item", ge=1, lt=3),
+    q: str = Query(default="something", max_length=20),
+):
+    items = {1: "item 1", 2: "item 2"}
+    return [items[item_id], q]
 
 
 @app.get("/models/{model_name}")
@@ -73,7 +162,7 @@ def get_post_list():
 
 
 @app.post("/posts/")
-def create_post(post: Post):
+def create_post(post: Post = Body(embed=True)):
     return post.dict()
 
 
